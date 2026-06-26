d2b368fe16c5e7997be12c0f632fdfefa8ff1174
a14509-oficina <a14509@oficina.pt>
2026-06-26 13:24:22 +0000

    Atualizar credenciais InfinityFree e adicionar .htaccess

--- config/database.php
+++ config/database.php
@@ -7,7 +7,7 @@
 //   define('DB_USER', 'root');
 //   define('DB_PASS', '');
 
-define('DB_HOST', 'sql300.infinityfree.com');
-define('DB_NAME', 'if0_42203983_devbank');
-define('DB_USER', 'if0_42203983');
-define('DB_PASS', 'kd7texfgXcf9n');
+define('DB_HOST', 'sql105.infinityfree.com');
+define('DB_NAME', 'if0_42271845_devbank');
+define('DB_USER', 'if0_42271845');
+define('DB_PASS', 'buJKYL967syAuM');
